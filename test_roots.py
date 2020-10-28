70231db80183d5ebbe1c7495904827fd3f5e988c
maxli0728 <54636428+maxli0728@users.noreply.github.com>
2020-10-28 09:38:49 +0800

    new files added

---
+++ test_roots.py
@@ -0,0 +1,27 @@
+# Problem 5 -- Continuous Integration -- for Homework 4 of CS107
+# Adapted from Lecture 12
+
+import pytest
+import roots
+
+def test_quadroots_result():
+    assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
+
+def test_quadroots_types():
+    with pytest.raises(TypeError):
+        roots.quad_roots("", "green", "hi")
+
+def test_quadroots_zerocoeff():
+    with pytest.raises(ValueError):
+        roots.quad_roots(a=0.0)
+
+def test_linearoots_result():
+    assert roots.linear_roots(2.0, -3.0) == 1.5
+
+def test_linearroots_types():
+    with pytest.raises(TypeError):
+        roots.linear_roots("ocean", 6.0)
+
+def test_linearroots_zerocoeff():
+    with pytest.raises(ValueError):
+        roots.linear_roots(a=0.0)
